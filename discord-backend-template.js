04e4eb2afd4676dca581857f54a9d450c8043a76
5aramin <elikarlssonn@gmail.com>
2026-05-31 15:45:49 +0200

    Update discord-backend-template.js

--- discord-backend-template.js
+++ discord-backend-template.js
@@ -280,6 +280,6 @@ const server = http.createServer(async (req, res) => {
   }
 });
 
-server.listen(PORT, "127.0.0.1", () => {
-  console.log(`Staff panel server running on http://127.0.0.1:${PORT}`);
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Staff panel server running on port ${PORT}`);
 });
